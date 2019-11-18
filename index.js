0320a719c811406489dc13de5e36ec5d1a1984ab
fhalbiero <fhalbiero@gmail.com>
2019-11-18 11:03:07 -0300

    alterações no design do form

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const { promisify} = require('util');
+const { promisify } = require('util');
 const sgMail = require('@sendgrid/mail');
 
 const GoogleSpreadsheet = require('google-spreadsheet');
